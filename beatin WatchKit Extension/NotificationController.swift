315574425ea338922d565fabb8157112e2e7e8e7
Nadia Bordoni <nadiabordoni@MacBook-Pro-de-Nadia.local>
2021-06-01 17:23:00 -0300

    cria notificação

--- beatin WatchKit Extension/NotificationController.swift
+++ beatin WatchKit Extension/NotificationController.swift
@@ -10,24 +10,41 @@ import SwiftUI
 import UserNotifications
 
 class NotificationController: WKUserNotificationHostingController<NotificationView> {
-
+    var title: String?
+    var message: String?
+    
     override var body: NotificationView {
-        return NotificationView()
+        return NotificationView(title: title, message: message)
+        
     }
-
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
-
+    
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    
+    
     override func didReceive(_ notification: UNNotification) {
         // This method is called when a notification needs to be presented.
         // Implement it if you use a dynamic notification interface.
         // Populate your dynamic notification interface as quickly as possible.
+        let okButton = NSLocalizedString("OK", comment: "Done button title in notification.")
+        notificationActions = [UNNotificationAction(identifier: "okButton", title: okButton, options: [])]
+            
+        
+        let notificationData =
+            notification.request.content.userInfo as? [String: Any]
+        
+        let aps = notificationData?["aps"] as? [String: Any]
+        let alert = aps?["alert"] as? [String: Any]
+        
+        title = alert?["title"] as? String
+        message = alert?["body"] as? String
     }
 }
+
--- beatin WatchKit Extension/NotificationView.swift
+++ beatin WatchKit Extension/NotificationView.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct NotificationView: View {
+    var title: String?
+    var message: String?
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack(alignment: .leading) {
+            Text(title ?? "hello")
+                .font(/*@START_MENU_TOKEN@*/.body/*@END_MENU_TOKEN@*/)
+                .fontWeight(.bold)
+                .padding(1.0)
+            Text(message ?? "hi")
+            
+        }
+        
+        
     }
 }
 
 struct NotificationView_Previews: PreviewProvider {
     static var previews: some View {
         NotificationView()
+            
     }
 }
