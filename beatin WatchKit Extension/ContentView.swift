3aaa31797ee2fb71c6c7fa3ba0c99374b02ffab0
Vitinho 21 <vbluis.play@gmail.com>
2021-06-04 08:39:58 -0300

    retira navigation link

--- beatin WatchKit Extension/ContentView.swift
+++ beatin WatchKit Extension/ContentView.swift
@@ -13,38 +13,36 @@ struct ContentView: View {
     
     var body: some View {
         
-            NavigationView {
+        
+        
+        VStack() {
             
-            VStack() {
-                                
-                NavigationLink(
-                    destination: PlayerView(),
-                    label: {
-                        Image("heart")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    })
-
-                Text("83 BPM")
-                    .font(.system(size: 24))
+            
+            Image("heart")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            
+            
+            Text("83 BPM")
+                .font(.system(size: 24))
+                .foregroundColor(.lightPurple)
+            
+            HStack(){
+                Text("nome da musica q")
+                    .font(/*@START_MENU_TOKEN@*/.subheadline/*@END_MENU_TOKEN@*/)
                     .foregroundColor(.lightPurple)
-                
-                HStack(){
-                    Text("nome da musica q")
-                        .font(/*@START_MENU_TOKEN@*/.subheadline/*@END_MENU_TOKEN@*/)
-                        .foregroundColor(.lightPurple)
-                        .multilineTextAlignment(.leading)
-                    Image("musicalNote")
-                        .resizable()
-                        .frame(width: 13, height: 15, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                }
-                
+                    .multilineTextAlignment(.leading)
+                Image("musicalNote")
+                    .resizable()
+                    .frame(width: 13, height: 15, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             }
-
             
-            }
-
+        }
+        
+        
+        
+        
         
     }
     
